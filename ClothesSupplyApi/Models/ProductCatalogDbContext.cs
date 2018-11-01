9bd700a104b1db9ddbb399aa5aa859b480dddaeb
abdullah.demirtas <ceabdullahdemirtas@gmail.com>
2018-11-01 23:23:31 +0300

    update

--- ClothesSupplyApi/Models/ProductCatalogDbContext.cs
+++ ClothesSupplyApi/Models/ProductCatalogDbContext.cs
@@ -17,15 +17,6 @@ namespace ClothesSupplyApi.Models
 
         public virtual DbSet<Product> Product { get; set; }
 
-        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //        {
-        //            if (!optionsBuilder.IsConfigured)
-        //            {
-        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-        //                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProductCatalogDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-        //            }
-        //        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product>(entity =>
