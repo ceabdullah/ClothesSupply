f99497905313223c42c36d259effe8e760005f87
abdullah.demirtas <ceabdullahdemirtas@gmail.com>
2018-11-01 22:50:56 +0300

    Add project files.

---
+++ ClothesSupplyApi/Models/ProductCatalogDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ClothesSupplyApi.Models
+{
+    public partial class ProductCatalogDbContext : DbContext
+    {
+        public ProductCatalogDbContext()
+        {
+        }
+
+        public ProductCatalogDbContext(DbContextOptions<ProductCatalogDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Product { get; set; }
+
+        //        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //        {
+        //            if (!optionsBuilder.IsConfigured)
+        //            {
+        //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+        //                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ProductCatalogDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        //            }
+        //        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.LastUpdated).HasColumnType("datetime");
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+        }
+    }
+}
